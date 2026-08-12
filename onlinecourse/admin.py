e53ec52a2a42352574522b6c6536464230d74462
tharunpullur2005-creator <tharunpulluru2005@gmail.com>
2026-08-12 10:48:22 -0400

    Update course detail bootstrap template

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -25,12 +25,12 @@ class ChoiceInline(admin.StackedInline):
 		model = Choice
 		extra = 2
 
-	class QuestionInline(admin.StackedInline):
+class QuestionInline(admin.StackedInline):
 		model = Question
 		extra = 2
 
 class QuestionAdmin(admin.ModelAdmin):
-		inlines = [Question_sub_content]
+		inlines = [ChoiceInline]
 		list_display = ['content']
 
 
